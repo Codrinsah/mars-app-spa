e9891d986d5e2fd40a7e75d805a1b1796dc25a1e
Codrin Cotarlan <codrin.cotarlan@softwire.com>
2021-07-01 13:18:11 +0100

    Implemented local storage for the click counter

--- mars-app-spa/src/ClickCounter.tsx
+++ mars-app-spa/src/ClickCounter.tsx
@@ -1,10 +1,22 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const ClickCounter: React.FC = () => {
     const [clickCount, setClickCount] = useState(0);
 
+    useEffect(() => {
+        const count = localStorage.getItem('clickCount');
+        if (!count) {
+            localStorage.setItem('clickCount', '0');
+        } else {
+            setClickCount(parseInt(count));
+        }
+    },
+        []);
+
     function onClick() {
-        setClickCount(clickCount + 1);
+        const nextClickCount = clickCount + 1;
+        localStorage.setItem('clickCount', nextClickCount.toString())
+        setClickCount(nextClickCount);
     }
     return <button onClick={onClick}> This button was clicked {clickCount} times! </button>
 };
