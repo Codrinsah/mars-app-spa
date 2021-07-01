5acbbe14aa08354c9143427a7784869e59ba01ae
Codrin Cotarlan <codrin.cotarlan@softwire.com>
2021-07-01 12:52:24 +0100

    Added a NASA card component

---
+++ mars-app-spa/src/NasaCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NasaCard: React.FC = () => {
+    return(
+        <div>
+            <h1> Title </h1>
+            <img src = "Image link goes here!"/>
+            <p> Paragraph 1 </p>
+            <p> Paragraph 2 </p>
+        </div>
+    )
+};
+
+export {NasaCard}
